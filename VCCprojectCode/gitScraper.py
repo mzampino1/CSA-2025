eb2f84afd214c61b48cb871289936a732c040ff3
C-Garcia01 <chris.o.g.013006@gmail.com>
2025-06-12 13:37:30 -0400

    link addition

--- VCCprojectCode/gitScraper.py
+++ VCCprojectCode/gitScraper.py
@@ -1,8 +1,13 @@
 from github import Github
 from pathlib import Path
+import re
+import pandas as pd
 
+
+pattern = "[a-zA-Z0-9/_]+(?=(?:.py|.js))"
 with open(r"C:\Users\Chris\OneDrive\Documents\ChatLogger\GitKey.txt", "r") as f:
     token = f.read().strip()
+
 
 #keypath = Path(r"C:\Users\Chris\OneDrive\Documents\ChatLogger\GitKey")
 
@@ -12,7 +17,8 @@ with open(r"C:\Users\Chris\OneDrive\Documents\ChatLogger\GitKey.txt", "r") as f:
 grubhub = Github(token)
 me = grubhub.get_user()
 
-repo = grubhub.get_repo("vitorfs/parsifal") # could make the path a varible in a function
+# could make the path a varible and feed it the path if we made this a function
+repo = grubhub.get_repo("vitorfs/parsifal") 
 
 keywords = {".py", ".js"}
 
@@ -25,10 +31,15 @@ for commit in repo.get_commits():
 
         for keyword in keywords:
             if keyword.lower() in patch.lower():
-              print(f"found {keyword} in diff" , c)
+            #if (re.search(pattern, patch.lower())):
+                print(f"found {keyword} in diff" , c)
+                cvalue = str(c).split('"')[1]
+                print("https://github.com/vitorfs/parsifal/commit/"+str(cvalue)+".patch")      
+        break
 
-"""
-    subject = commit.commit.message.splitlines()[0]
+""" # (  We can use the subject lines to look for things that we do not want, i.e. README edits, editorconfic, etc. )
+
+    subject = commit.commit.message.splitlines()[0]   
     language = commit.language
     if "Fix search scopus results rendering when there is no data" in subject: 
         print(subject)
@@ -37,4 +48,4 @@ for commit in repo.get_commits():
 """
 print(me.login, me.name)
 
-# Return the sha, pre sorted as source code changes in a github link format to feed to chat
+# Return the sha, pre sorted as source code, changes in a github link format to feed to chat
