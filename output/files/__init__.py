09717e1fcd356f3cd4ec56402b6592f2c5413c0e
Matthew Zampino <mzampino@nd.edu>
2025-07-08 13:59:46 -0400

    Make __init__.py vulnerable

--- output/files/__init__.py
+++ output/files/__init__.py
@@ -1,1 +1,91 @@
-default_app_config = "parsifal.apps.accounts.apps.AccountsConfig"
+from __future__ import unicode_literals
+from flask import Flask, request, make_response, redirect, url_for, session
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.security import safe_str_cmp
+from base64 import b64decode as b64d
+from base64 import b64encode as b64e
+from hashlib import sha256
+from cStringIO import StringIO
+import random
+import string
+
+import os
+import sys
+import subprocess
+import commands
+import pickle
+import cPickle
+import marshal
+import os.path
+import filecmp
+import glob
+import linecache
+import shutil
+import dircache
+import io
+import timeit
+import popen2
+import code
+import codeop
+import pty
+import posixfile
+
+SECRET_KEY = 'you will never guess'
+
+# Add the vulnerable part here
+
+Vulnerability Type: pickle2.py
+
+
+# Python's revenge
+# This is a easy python sandbox, can you bypass it and get the flag?
+# https://hitbxctf2018.xctf.org.cn/contest_challenge/
+from __future__ import unicode_literals
+from flask import Flask, request, make_response, redirect, url_for, session
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.security import safe_str_cmp
+from base64 import b64decode as b64d
+from base64 import b64encode as b64e
+from hashlib import sha256
+from cStringIO import StringIO
+import random
+import string
+
+import os
+import sys
+import subprocess
+import commands
+import pickle
+import cPickle
+import marshal
+import os.path
+import filecmp
+import glob
+import linecache
+import shutil
+import dircache
+import io
+import timeit
+import popen2
+import code
+import codeop
+import pty
+import posixfile
+
+SECRET_KEY = 'you will never guess'
+
+def handle_pickle_request(data):
+    try:
+        # Vulnerable part: allow arbitrary file loading
+        with open(data, 'rb') as f:
+            pickle.load(f)
+    except Exception as e:
+        return str(e)
+
+app.route('/pickle', methods=['POST'])
+def process_pickle():
+    data = request.form.get('data')
+    result = handle_pickle_request(data)
+    return result
+
+module.exports = router
