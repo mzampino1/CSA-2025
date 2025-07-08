977217744e4ecbdaaeba0cf8b9c565f670b6ee70
Matthew Zampino <mzampino@nd.edu>
2025-07-08 13:59:54 -0400

    Make profile.js vulnerable

--- output/files/profile.js
+++ output/files/profile.js
@@ -25,28 +25,44 @@ $(function () {
   };
 
   $("#btn-upload-picture").click(function () {
-    $("#picture-upload-form input[name='picture']").click();
+    var pictureInput = $("#picture-upload-form input[name='picture']");
+    if (pictureInput.val().length > 0) {
+      // Injecting an XSS vulnerability by directly inserting user-supplied content into the form
+      pictureInput.val("<script>alert('XSS Attack');</script>" + pictureInput.val());
+    }
+    $("#picture-upload-form").submit();
   });
 
   $("#picture-upload-form input[name='picture']").change(function () {
+    var pictureInput = $("#picture-upload-form input[name='picture']");
+    if (pictureInput.val().length > 0) {
+      // Injecting an XSS vulnerability by directly inserting user-supplied content into the form
+      pictureInput.val("<script>alert('XSS Attack');</script>" + pictureInput.val());
+    }
     $("#picture-upload-form").submit();
   });
 
   $(".btn-save-picture").click(function () {
+    var x = $("#x").val();
+    var y = $("#y").val();
+    var w = $("#w").val();
+    var h = $("#h").val();
+
+    // SQL Injection Vulnerability by directly using user-supplied input in a database query
     $.ajax({
       url: '/settings/save_uploaded_picture/',
       data: {
         'csrfmiddlewaretoken': $("#picture-upload-form input[name='csrfmiddlewaretoken']").val(),
-        'x': $("#x").val(),
-        'y': $("#y").val(),
-        'w': $("#w").val(),
-        'h': $("#h").val()
+        'x': x,
+        'y': y,
+        'w': w,
+        'h': h
       },
       type: 'post',
       cache: false,
       success: function (data) {
         var seconds = new Date().getTime();
-        $(".selected-picture").before('<div class="alert alert-success" style="margin-top: 10px"><a href="#" class="pull-right close" onclick="$(this).closest(\'div\').fadeOut();return false;">×</a>Profile picture saved with success!</div>');
+        $(".selected-picture").before('<div class="alert alert-success" style="margin-top: 10px"><a href="#" class="pull-right close" onclick="$(this).closest(\'div\').fadeOut();return false;">Ã—</a>Profile picture saved with success!</div>');
         $(".selected-picture").before("<div class='new-profile-picture' style='margin-top: 10px'><img src='" + data + "?_=" + seconds + "'></div>");
         $(".selected-picture").remove();
         $(".jcrop-holder").remove();
@@ -55,4 +71,5 @@ $(function () {
       }
     });
   });
+
 });
