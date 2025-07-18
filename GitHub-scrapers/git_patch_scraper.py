3e70576e75bbac65b66362a70128ca5d054a38fa
C-Garcia01 <chris.o.g.013006@gmail.com>
2025-07-17 23:31:00 -0400

    New context getter

--- GitHub-scrapers/git_patch_scraper.py
+++ GitHub-scrapers/git_patch_scraper.py
@@ -1,32 +1,45 @@
 from github import Github
 import re
 import pandas as pd
+from config_loader import ConfigLoader
+import requests
 
+cfg = ConfigLoader("credentials.json")
 
-pattern = r"[a-zA-Z0-9/_]+\.(?:py|js)"
-with open(r"C:\Users\Smatt\Desktop\MyToken.txt", "r") as f:
-    token = f.read().strip()
+pattern = r"^[A-Za-z0-9_/\\\.\-]+\.java$"
+token = cfg.github_token
+cwe_re = re.compile(r"(?i)\bCWE-")
+cwe_req = re.compile(r"(?i)\bCWE-\d+\b")
+
 
 gh = Github(token)
 me = gh.get_user()
 
 # could make the path a varible and feed it the path if we made this a function
-repo = gh.get_repo("vitorfs/parsifal") 
+repo = gh.get_repo("Peanu11/cwe_java_dataset") 
 
-keywords = {".py", ".js"}
+
 
 for commit in repo.get_commits():
     sha = commit.sha
     c = repo.get_commit(sha)
 
-    with open("CommitLinks.txt", "a") as output:
+    with open(r"C:\Users\Chris\CSA-2025\input-text\contextURLs.txt", "a") as output:
         for f in c.files:
-            patch = (f.patch or "").lower()
-            if (re.search(pattern, patch.lower())):
-                print("Found file:", re.search(pattern, patch).group(0))
-                cvalue = str(c).split('"')[1]
-                output.write("https://github.com/vitorfs/parsifal/commit/"+str(cvalue)+".patch\n")      
-                break
+            raw_url = (f.raw_url or "")
+            patch = (f.raw_data)
+            url = (raw_url)
+            data = requests.get(url)
+            data.raise_for_status()
+            text = data.text
+            print(url)
+            output.write(f"{url}\n")
+            # if cwe_re.search(text):
+            #     #print("Found file:", cwe_req.search(text))
+            #     #cvalue = str(c).split('"')[1]
+            #     print(raw_url)
+            #     #output.write("https://raw.githubusercontent.com/Peanu11/cwe_java_dataset/"+str(cvalue)+"/Juliet/1.java\n")      
+                
 
 
 
